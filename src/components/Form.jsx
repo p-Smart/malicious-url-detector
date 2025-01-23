7baf9e36993535e1f3985b6e3991fab04192deb6
pSmart <princeayokunle2002@gmail.com>
2025-01-23 15:52:09 +0100

    feat: add api integration

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,11 +6,13 @@ import {
   Input,
   Select,
   Stack,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
 import { useState } from "react";
-import delay from "../utils/delay";
 import ResultDisplay from "./ResultDisplay";
+import axios from "axios";
+import delay from "../utils/delay";
 
 const Form = () => {
   const [loading, setLoading] = useState(false);
@@ -20,17 +22,75 @@ const Form = () => {
   };
   const [data, setData] = useState(initialData);
   const [response, setResponse] = useState(null);
+  const toast = useToast();
 
   const handleSubmit = async () => {
     try {
       setLoading(true);
-      await delay(2000);
-      setResponse("A response");
+      const loadingMessages = [
+        "Fetching pickle...",
+        "Loading vectorizer...",
+        "Applying SVM model...",
+        "Generating predictions...",
+        "Decoding prediction results...",
+      ];
+      let messageIndex = 0;
+      let intervalId;
+      const toastId = toast({
+        title: loadingMessages[messageIndex],
+        status: "loading",
+        position: "top",
+        duration: null,
+        isClosable: false,
+      });
+
+      intervalId = setInterval(() => {
+        messageIndex = messageIndex + 1;
+        toast.update(toastId, {
+          title: loadingMessages[messageIndex] || loadingMessages[4],
+          status: "loading",
+        });
+      }, 1000);
+
+      const { data: responseData } = await axios.post(
+        "https://malicious-url-detector-backend.onrender.com/check-url",
+        {
+          url: data.url,
+          model: data.model,
+        }
+      );
+
+      if (!responseData?.success) {
+        toast.close(toastId);
+        throw new Error(responseData?.message);
+      }
+
+      setResponse(responseData.data);
+
+      clearInterval(intervalId);
+      toast.update(toastId, {
+        title: "Prediction completed successfully!",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
     } catch (err) {
-      console.log(err);
+      toast({
+        title: err?.response?.data?.message || err.message,
+        status: "error",
+        position: "top",
+      });
     } finally {
       setLoading(false);
     }
+  };
+
+  const modelMap = {
+    svm: "Support Vector Machine (SVM)",
+    knn: "K-Nearest Neighbors (KNN)",
+    random_forest: "Random Forest (RF)",
+    naive_bayes: "Naive Bayes (NB)",
+    xgboost: "XGBoost",
   };
 
   return (
@@ -56,6 +116,7 @@ const Form = () => {
           <FormControl>
             <FormLabel>URL</FormLabel>
             <Input
+              disabled={response}
               type="url"
               placeholder="Enter the URL to check"
               focusBorderColor="brand.400"
@@ -67,6 +128,7 @@ const Form = () => {
           <FormControl>
             <FormLabel>Model to Use</FormLabel>
             <Select
+              disabled={response}
               placeholder="Select a model"
               focusBorderColor="brand.400"
               value={data.model}
@@ -75,12 +137,13 @@ const Form = () => {
             >
               <option value="svm">Support Vector Machine (SVM)</option>
               <option value="knn">K-Nearest Neighbors (KNN)</option>
-              <option value="rf">Random Forest (RF)</option>
-              <option value="nb">Naive Bayes (NB)</option>
-              <option value="xgb">XGBoost</option>
+              <option value="random_forest">Random Forest (RF)</option>
+              <option value="naive_bayes">Naive Bayes (NB)</option>
+              <option value="xgboost">XGBoost</option>
             </Select>
           </FormControl>
           <Button
+            disabled={response}
             type="submit"
             colorScheme="brand"
             size="lg"
@@ -88,10 +151,21 @@ const Form = () => {
           >
             Detect URL
           </Button>
+          {response ? (
+            <Button mt="20px" ml="auto" onClick={() => setResponse(null)}>
+              Reset
+            </Button>
+          ) : null}
         </VStack>
       </Box>
 
-      {response ? <ResultDisplay url={data.url} /> : null}
+      {response ? (
+        <ResultDisplay
+          url={data.url}
+          prediction={response?.prediction}
+          model={modelMap[data.model]}
+        />
+      ) : null}
     </Stack>
   );
 };
--- src/components/ResultDisplay.jsx
+++ src/components/ResultDisplay.jsx
@@ -1,8 +1,17 @@
 import { Box, Text } from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
 
-const ResultDisplay = ({ url }) => {
+const ResultDisplay = ({ url, prediction, model }) => {
+  const ref = useRef(null);
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [ref.current]);
+
   return (
     <Box
+      ref={ref}
       bg="whiteAlpha.800"
       borderRadius="md"
       boxShadow="lg"
@@ -15,13 +24,16 @@ const ResultDisplay = ({ url }) => {
       <Text fontSize="lg" fontWeight="bold">
         The URL{" "}
         <Text as="span" color="brand.500">
-          {url || "https://example.com"}
+          {url}
         </Text>{" "}
         is detected to be a{" "}
-        <Text as="span" color="green.500">
-          benign URL
+        <Text
+          as="span"
+          color={prediction === "benign" ? "green.500" : "red.500"}
+        >
+          {prediction} URL{" "}
         </Text>
-        (80% accuracy).
+        using the {model} model.
       </Text>
     </Box>
   );
